7c9144a8215037481a92e31bf4a134d44f2d28d1
angr0316 <fevuvu26>
2016-05-20 12:33:20 +0300

    fixes with dependencies

--- persistence/src/test/scala/StorageComponentImpl$Test.scala
+++ persistence/src/test/scala/StorageComponentImpl$Test.scala
@@ -1,36 +1,27 @@
-
-
-import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator.MemoryLayoutSpecification
-import org.scalatest.BeforeAndAfterAll
-
+import org.specs2.specification._
 import scalalab3.lyricsengine.domain.{DataSet, Song}
 import scalalab3.lyricsengine.persistence.StorageComponentImpl
-import java.time.OffsetDateTime
 import java.util.UUID
-import org.scalatest
 import org.specs2.mutable.Specification
 
-import scalalab3.lyricsengine.persistence.mongo.{MongoContext,MongoConfig}
+import scalalab3.lyricsengine.persistence.mongo.{MongoContext, MongoConfig}
 
 import scala.util.Try
 
-class StorageComponentImpl$Test extends Specification with BeforeAndAfterAll {
+class StorageComponentImpl$Test extends Specification with BeforeAfterAll {
   sequential
 
   val tryMongoContext = Try(new MongoContext(MongoConfig.load()))
 
-
- // val tryMongo = Try(MongoConnection("localhost", 12345))
-
   "Mongo Test" >> {
     if (tryMongoContext.isSuccess) {
-      implicit val s=tryMongoContext.get
-      val now = OffsetDateTime.now()
+      implicit val s = tryMongoContext.get
+
       val words = Map(1 -> "i", 2 -> "the", 3 -> "you", 4 -> "to", 5 -> "and")
       val firstSong = Song("TRZZZYV128F92E996D", "6849828", Map(1 -> 10, 2 -> 6, 3 -> 20, 5 -> 2, 7 -> 30))
       val secondSong = Song("TRZZZYX128F92D32C6", "681124", Map(1 -> 4, 2 -> 18, 4 -> 3, 5 -> 6, 6 -> 9))
-      val seqSong=Seq(firstSong, secondSong)
-      DataSet(words,seqSong )
+      val seqSong = Seq(firstSong, secondSong)
+      DataSet(words, seqSong)
       val mongoStorage = new StorageComponentImpl {
         override val storage: Storage = new StorageImpl
       }.storage
@@ -44,21 +35,20 @@ class StorageComponentImpl$Test extends Specification with BeforeAndAfterAll {
         mongoStorage.addWordsDefinition(words)
       }
 
-      "find Songs" in{
-       mongoStorage.findSongs()
+      "find Songs" in {
+        mongoStorage.findSongs()
       }
 
-      "find WordsDefinitions" in{
+      "find WordsDefinitions" in {
         mongoStorage.findWordsDefinitions()
       }
 
-
-
-
-    }else "Skipped Test" >> skipped ("MongoDB is not available in ")
+    } else "Skipped Test" >> skipped("MongoDB is not available in ")
   }
+
   def uuid() = Some(UUID.randomUUID())
-  def drop() = for (m <- tryMongoContext) m.dbDrop()
+
+  def drop() = for (m <- tryMongoContext) m.drop
 
   override def beforeAll(): Unit = drop()
   override def afterAll(): Unit = drop()
